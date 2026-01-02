83098171be04b805d1d8fb42afd11c10c02176d9
Pierre GALLIOT <pierre.galliot.pg@outlook.fr>
2026-01-02 13:55:54 +0100

    Implement UAC elevation and UEFI password setting script

--- run_uefi_script.bat
+++ run_uefi_script.bat
@@ -1,20 +1,2 @@
 @echo off
-
-:: --- Vérification des droits ---
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo Demande de privileges administrateur...
-    goto :UACPrompt
-) else ( goto :is_admin )
-
-:UACPrompt
-    :: Crée un petit script temporaire pour relancer le BAT en admin
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:is_admin
-powershell.exe -ExecutionPolicy Bypass -File "MonScript.ps1"
-pause
+powershell.exe -ExecutionPolicy Bypass -File "src/uefiSetter.ps1"
---
+++ src/uefiSetter.ps1
@@ -0,0 +1,67 @@
+### Elevation ###
+
+# Get the ID and security principal of the current user account
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole))
+{
+    # We are running "as Administrator" - so change the title and background color to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+    clear-host
+}
+else
+{
+    # We are not running "as Administrator" - so relaunch as administrator
+
+    # Create a new process object that starts PowerShell
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+    # Specify the current script path and name as a parameter
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+
+    # Exit from the current, unelevated, process
+    exit
+}
+
+### UEFI password setting ###
+
+$scriptPath = $PSScriptRoot
+
+function getManufacturer {
+    $computerSystem = Get-WmiObject -Class Win32_ComputerSystem
+    return $computerSystem.Manufacturer
+}
+
+function setPasswordHP {
+    param (
+        [string]$password
+    )
+    & "$scriptPath\HPQPswd.exe" /s /p"$password" /f"$scriptPath\mdp.bin"
+    & "$scriptPath\BiosConfigUtility.exe /npwdfile:"mdp.bin"
+}
+
+$manufacturer = getManufacturer
+Write-Host "Computer Manufacturer: $manufacturer"
+
+if ($manufacturer -like "HP*") {
+    $password = Read-Host "Enter the UEFI password to set"
+    setPasswordHP -password $password
+} elseif ($manufacturer -like "Dell") {
+    Write-Host "Dell UEFI password setting is not yet implemented."
+} else {
+    Write-Host "Unsupported manufacturer: $manufacturer"
+}
+Write-Host -NoNewLine "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
